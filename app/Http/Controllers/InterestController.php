6832425673b823ea7f562ca7f012f213ecbb43a4
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2025-01-05 15:28:28 -0600

    release: se agrega cálculo de resultados de interés por años/mes

--- app/Http/Controllers/InterestController.php
+++ app/Http/Controllers/InterestController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CalculateDataRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Support\Carbon;
 
 class InterestController extends Controller
 {
-    public function calculateCompoundInterest(CalculateDataRequest $request) {
+    /**
+     * Calculate compound interest by year
+     * @param CalculateDataRequest $request
+     * @return JsonResponse
+     * @throws BindingResolutionException
+     */
+    public function calculateByYear(CalculateDataRequest $request) {
         $capitalInicial = $request->capitalInicial;
         $interes = $request->interes;
         $anios = $request->anios;
@@ -18,6 +27,38 @@ class InterestController extends Controller
                 'year' => $year,
                 'capital' => $capitalInicial * (1 + $interes / 100) ** $year
             ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'OK',
+            'data' => $result
+        ]);
+    }
+    /**
+     * Calculate compound interest by month
+     * @param CalculateDataRequest $request
+     * @return JsonResponse
+     * @throws BindingResolutionException
+     */
+    public function calculateByMonth(CalculateDataRequest $request) {
+        $capitalInicial = $request->capitalInicial;
+        $interes = $request->interes;
+        $anios = $request->anios;
+        $result = collect();
+
+        $counterMonth = 1;
+        for ($year = 1; $year <= (int)$anios; $year++) {
+            for ($month = 1; $month <= 12; $month++) {
+                $counterMonth++;
+                $result->push([
+                    'year' => $year,
+                    'month' => $counterMonth,
+                    'montfOfTheYear' => $month,
+                    'monthName' => getMonthName($month),
+                    'capital' => $capitalInicial * (1 + $interes / 100) ** ($year * $month / (12 * $year))
+                ]);
+            }
         }
 
         return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\InterestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/calculate-compount-interes', [InterestController::class, 'calculateCompoundInterest']);
+Route::group(['prefix' => '1.0.0'], function () {
+    Route::group(['prefix' => 'compound-interest'], function () {
+        Route::get('/year', [InterestController::class, 'calculateByYear']);
+        Route::get('/month', [InterestController::class, 'calculateByMonth']);
+    });
+});
 
 Route::get('/', function () {
     return response()->json([
